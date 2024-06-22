eb37d4db50f487dd832b99515459f9b374625140
Mahamudul Hasan <124416655+MahamudulHasanAbid@users.noreply.github.com>
2024-06-22 12:27:32 +0600

    Add files via upload

---
+++ practice_for_loop.py
@@ -0,0 +1,54 @@
+# sum = 0
+
+# for i in range(2, 22, 2):
+#     sum +=i
+# print(sum)
+# -----------------------------------------------------
+# num = [2,4,5,7,8,12]
+
+# for i in num:
+#     squr = i ** 2
+#     print("Square of ", i ,"is : ", squr)
+# -----------------------------------------------------
+# for i in range(1, 11):
+#     if i%2 == 0:
+#         print(i," is an even number.")
+
+#     else:
+#         print(i, " is an odd number.")
+
+# -----------------------------------------------------
+
+# Use for loop to generate a list of numbers from 9 to 50 divisible by 2
+# li = []
+# for i in range(9, 51):
+#     if i % 2 == 0:
+#         li.append(i)
+# print(li)
+
+# -----------------------------------------------------
+# Print Multiplication table of 5 numbers using for loop
+# for i in range(1,6):
+#     print("Multiplication table of ", i)
+
+#     count = 1;
+#     while count<13:
+#         print(i ,"*", count, "= ", i*count)
+#         count += 1
+    
+#     print('\n')
+# -----------------------------------------------------
+
+# numList = [23,44,66,11,34,91]
+# size = len(numList)
+# for i in range(size):
+#     print("Index: ", i, "value:", numList[i])
+# # using enumerate function
+# num = [23,44,66,11,34,91]
+
+# for i, j in enumerate(num):
+#     print('Index: ', i, "Value: ",j)
+
+# -----------------------------------------------------
+
+# help("keywords")
